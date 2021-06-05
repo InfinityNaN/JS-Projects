25e70de26850b82a3f303f1a953c68a9356ed8be
Narayanprajapat <narayanprajapat00099@gmail.com>
2021-06-05 23:35:57 +0530

    JS Update

--- Tipping/script.js
+++ Tipping/script.js
@@ -1,1 +1,11 @@
+// querySelector, addEventListner, innerHTMl, innerText
+var btn = document.querySelector('button');
+var inp = document.querySelector('input');
+var output = document.querySelector('.output');
+// console.log(btn);
+btn.addEventListener('click', showMessage);
 
+function showMessage() {
+	// console.log(`Welcome to the team ${inp.value}`);
+	output.innerHTML = `Donate 15% Of Our Salary For Education :  ${((inp.value * 15) / 100).toFixed(2)}`;
+}
