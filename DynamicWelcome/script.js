c377bc416b8366cd7b8809ac3c2531e7ad61f329
Narayanprajapat <narayanprajapat00099@gmail.com>
2021-06-13 23:51:33 +0530

    Greet

---
+++ DynamicWelcome/script.js
@@ -0,0 +1,15 @@
+const greet = () => {
+	let date = new Date();
+	let hr = date.getHours();
+	let out = '';
+	if (hr >= 22 && hr > 6) {
+		out = 'Hello Good Night';
+	} else if (hr >= 6 && hr < 12) {
+		out = 'Hello Good Morning';
+	} else if (hr >= 12 && hr < 17) {
+		out = 'Hello Good Afternoon';
+	} else if (hr >= 17 && hr < 22) {
+		out = 'Hello Good Evening';
+	}
+	document.querySelector('.output').innerHTML = out;
+};
