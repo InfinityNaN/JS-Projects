53fe21088ba5387f9ff400e410d65b7115b118d7
Abhishek Choudhary <abhishekchoudhary@Abhisheks-MacBook-Pro.local>
2021-06-14 19:28:23 +0530

    logic part generated

--- CoinToss/script.js
+++ CoinToss/script.js
@@ -1,1 +1,28 @@
 
+button = document.querySelectorAll("button")
+output = document.querySelector(".output")
+let userScore=0, computerScore=0
+let tossArray = ["Tail", "Head"]
+for(b of button){
+   b.addEventListener("click",flipCoin)
+}
+
+function flipCoin(e){
+    let tossResult = tossArray[parseInt(Math.random()*2)]  //head, tail
+    console.log(tossResult)
+    userChoice = (e.target.innerHTML).trim()
+    
+     winMessage = ""
+    if(tossResult == userChoice){
+        winMessage = "You won"
+        userScore++
+    }
+    else{
+        winMessage = "You Loss"
+        computerScore++
+    }
+    output.innerHTML = `<pre> UserScore = ${userScore}        ComputerScore = ${computerScore} </pre>
+                                              ${winMessage}`
+    
+    
+}
